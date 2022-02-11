c17b401ce1a51fbc5c4552bce5d7599e606310de
globalpolaris <nomadilog@gmail.com>
2022-02-11 00:55:50 -0800

    Revert "show total user"
    This reverts commit 47682e8a510356ba6887e91bd620f740a24066f7.

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Users</title>
-</head>
-
-<body>
-    <?php
-    $conn = mysqli_connect("192.168.75.129", "root", "asdasd", "trucorp", "6601");
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    $sql = "SELECT count(*) FROM users";
-    $result = $conn->query($sql);
-    if ($result) {
-        while ($row = $result->fetch_row()) {
-            echo "Total users: " . $row[0];
-        }
-    }
-
-
-    ?>
-</body>
-
-</html>
+<?php
+phpinfo();
+?>
